f8c3e86794e3acb0b0b44aeb0c30375c4532ecbd
wafflestomper <brian.zollinhofer@gmail.com>
2019-02-17 22:36:44 -0500

    create show action and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,13 @@ class UsersController < ApplicationController
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
     else
-
+      render 'edit'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    
   end
 
   private
